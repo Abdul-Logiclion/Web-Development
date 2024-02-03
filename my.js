9654abb7fefd457f8ca9a9082d1455432c8adcb1
Abdul Jabar Rahimoon <abduljabarsc01@gmail.com>
2024-02-03 13:32:41 +0500

    Initial commit

---
+++ my.js
@@ -0,0 +1,8 @@
+let noun1 = 'CNSDKJV';
+let adjective = 'silly';
+let noun2 = 'Jabr';
+let verb = 'do';
+let noun3 = 'rahimondjvnk';
+
+
+console.log(`The world's first ${noun1} was a very ${adjective} ${noun2} who loved to ${verb} while eating ${noun3} for every meal.`);
